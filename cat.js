9e5d8034cf38d6a175b1a7bcdfd1d42b8cb1001c
IvanTolstov <itolstof@gmail.com>
2017-03-13 17:22:45 +0300

    v 1

---
+++ cat.js
@@ -0,0 +1,22 @@
+
+// window.onscroll = function() {
+//   var block = document.querySelector('.banner');
+//   // var area = document.querySelector('.gap');
+//   // var showArea = area.getBoundingClientRect();
+//   // var coordsAd = block.getBoundingClientRect();
+//   var scrolled = window.pageYOffset;
+//   console.log(scrolled);
+//   if (scrolled > 80) {
+//     block.style.display = "block";
+//     for (i=0; i < 677; i++) {
+//       console.log(block.style.marginTop);
+
+//     }
+//   } 
+//   // else if (scrolled < 167) {
+//   //   block.style.display = "none";
+//   // }
+// }
+
+document.querySelector('.parallax-window').parallax({imageSrc:'http://babeholder.pixoil.com/img/375/667'});
+
